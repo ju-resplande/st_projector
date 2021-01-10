14bac93c8f7244c84898a514b4ff7c5217c210f3
Juliana Resplande <julianarsg13@gmail.com>
2021-01-10 13:47:24 -0300

    Add files via upload

---
+++ projector.py
@@ -0,0 +1,65 @@
+from argparse import ArgumentParser
+import os
+
+from sentence_transformers import SentenceTransformer
+from torch.utils.tensorboard import SummaryWriter
+import tensorboard as tb
+import tensorflow as tf
+import pandas as pd
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("st_projector")
+
+    parser.add_argument(
+        "--data",
+        action="store",
+        required=True,
+        help="Examples Tsv file with examples and labels",
+    )
+
+    parser.add_argument(
+        "--main_column",
+        action="store",
+        required=True,
+        help="Column containing sentences examples.",
+    )
+
+    parser.add_argument(
+        "--st_model",
+        action="store",
+        required=True,
+        help="Sentence",
+    )
+
+    parser.add_argument(
+        "--log_dir",
+        action="store",
+        required=True,
+        help="Tensorboard log_dir to keep embeddings data.",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
+
+    examples = df.read_csv(args.data, header=None)[args.main_column]
+    model = SentenceTransformer(args.st_model)
+    embeddings = model.encode(examples, convert_to_tensor=True)
+
+    writer = SummaryWriter(args.log_dir)
+    writer.add_embedding(embeddings, examples, tag=args.st_model)
+
+    metadata_path = os.path.join(args.log_dir, "00000", args.st_model, "metadata.tsv")
+    examples.to_csv(metadata_path, index=None, sep="\t")
+
+    program = tb.program.TensorBoard()
+    program.configure(argv=[None, "--logdir", args.log_dir])
+    program = tb.launch()
+
+
+if __name__ == "__main__":
+    main()
